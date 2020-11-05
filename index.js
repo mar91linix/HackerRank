349ec3342fd656e37c36eb12b9103d25ebc455e9
Your Name <you@example.com>
2020-11-04 22:37:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+// un forma
+// funtion de fleca
+    const clificaDesafio = (a, b) => {
+      
+      // las vairbles se decakararon en una misma -----no estab segura si si era la formula adecuada 
+      var[ a, b = 0]
+         
+      // cliclo for 
+      for (i = 0; i < a.length; i++) {
+          if ( a[i] > b[i] ) {
+              a+=1 
+          } else if ( a[i] < b[i]) {
+              contB+=1
+          } 
+      };
+  
+     a[0] = a;
+     b[1] = b;
+  
+      return result
+     
+  
+  };
+  console.log(clificaDesafio ['']);
+  
+
+
